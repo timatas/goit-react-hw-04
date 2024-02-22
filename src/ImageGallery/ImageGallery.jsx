e0e03941e3f96e9711a3b4a7f917a0392d5b4d14
timatas <139263028+timatas@users.noreply.github.com>
2024-02-22 23:03:34 +0200

    add Loader Cast Review

--- src/ImageGallery/ImageGallery.jsx
+++ src/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,6 @@
 import { ImageCard } from "../ImageCard/ImageCard";
 import css from "../ImageGallery/ImageGallery.module.css";
+
 export const ImageGallery = ({ items }) => {
   return (
     <ul className={css.list}>
